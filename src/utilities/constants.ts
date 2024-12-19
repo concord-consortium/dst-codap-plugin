b94dc4691c7f2ae259a7d70ef4b41fa05cb9feb6
Teale Fristoe <fristoe@gmail.com>
2024-12-19 08:50:35 -0800

    Move tabs into separate components.

---
+++ src/utilities/constants.ts
@@ -0,0 +1,10 @@
+export const kPluginName = "Space-Time Cube";
+export const kVersion = "0.0.1";
+export const kInitialDimensions = {
+  width: 380,
+  height: 680
+};
+export const kDataContextName = "SamplePluginData";
+
+export const kGraphTabLabel = "3D Graph";
+export const kAboutTabLabel = "About";
