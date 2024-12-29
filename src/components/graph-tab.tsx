84545eee2cbe655c3674762c967a6b82db6dda9c
Teale Fristoe <fristoe@gmail.com>
2024-12-28 17:40:58 -0800

    Add more buttons.

--- src/components/graph-tab.tsx
+++ src/components/graph-tab.tsx
@@ -1,9 +1,13 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
+import LegendIcon from "../assets/icons/display-hide-legend-icon.svg";
+import XYIcon from "../assets/icons/display-hide-xy-controls-icon.svg";
 import HomeIcon from "../assets/icons/home-icon.svg";
 import FitAllIcon from "../assets/icons/fit-all-icon.svg";
+import MarqueeIcon from "../assets/icons/marquee-select-icon.svg";
 import MinusIcon from "../assets/icons/minus.svg";
 import PlusIcon from "../assets/icons/plus.svg";
+import PointIcon from "../assets/icons/point-selection.svg";
 import { dstCamera } from "../models/camera";
 import { ScatterPlot } from "./plot/ScatterPlot";
 import { NavigationCube } from "./ui/navigation-cube";
@@ -30,6 +34,16 @@ export const GraphTab = observer(function GraphTab() {
           Icon={MinusIcon}
           onClick={() => dstCamera.setRadius(dstCamera.radius + 1)}
         />
+      </UIButtonContainer>
+      <UIButtonContainer className="mode-container">
+        <UIButton className="top" Icon={PointIcon} />
+        <UIButton className="bottom" disabled={true} Icon={MarqueeIcon} />
+      </UIButtonContainer>
+      <UIButtonContainer className="legend-container">
+        <UIButton className="top bottom" Icon={LegendIcon} />
+      </UIButtonContainer>
+      <UIButtonContainer className="xy-container">
+        <UIButton className="top bottom" Icon={XYIcon} />
       </UIButtonContainer>
     </div>
   );
