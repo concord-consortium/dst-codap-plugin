9f053a66ec39a0def374aa63c710bdeb96140052
Teale Fristoe <fristoe@gmail.com>
2025-01-13 08:20:24 -0800

    Prevent rotation if user dragged navigation cube.

--- src/components/ui/navigation-controls/navigation-cube-button.tsx
+++ src/components/ui/navigation-controls/navigation-cube-button.tsx
@@ -4,6 +4,11 @@ import { ThreeEvent } from "@react-three/fiber";
 import { Vector3 } from "three";
 import { dstCamera } from "../../../models/camera";
 
+interface clickPositionType {
+  x: number;
+  y: number;
+}
+
 interface INavigationCubeCornerProps {
   position: Vector3;
   size: [number, number, number];
@@ -12,12 +17,27 @@ interface INavigationCubeCornerProps {
 }
 export function NavigationCubeButton({ position, size, targetPivot, targetRotation }: INavigationCubeCornerProps) {
   const [hovering, setHovering] = useState(false);
+  const [clickPosition, setClickPosition] = useState<clickPositionType|null>(null);
   const opacity = hovering ? 0.5 : 0;
 
   const handleClick = (event: ThreeEvent<MouseEvent>) => {
     event.stopPropagation();
-    dstCamera.animateTo(dstCamera.targetZoom ?? dstCamera.zoom, targetPivot, targetRotation);
+
+    // Only rotate to the proper location if the user didn't drag the cube.
+    if (clickPosition) {
+      const distanceSquared = (event.clientX - clickPosition.x) ** 2 + (event.clientY - clickPosition.y) ** 2;
+      if (distanceSquared < 25) {
+        dstCamera.animateTo(dstCamera.targetZoom ?? dstCamera.zoom, targetPivot, targetRotation);
+      }
+    }
+    
+    setClickPosition(null);
     setHovering(false);
+  };
+
+  const handlePointerDown = (event: ThreeEvent<PointerEvent>) => {
+    event.stopPropagation();
+    setClickPosition({ x: event.clientX, y: event.clientY });
   };
 
   const handlePointerOver = (event: ThreeEvent<PointerEvent>) => {
@@ -35,6 +55,7 @@ export function NavigationCubeButton({ position, size, targetPivot, targetRotati
     <Box
       args={size}
       onClick={handleClick}
+      onPointerDown={handlePointerDown}
       onPointerOver={handlePointerOver}
       onPointerOut={handlePointerOut}
       position={position}
