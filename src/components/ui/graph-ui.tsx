62bcc89bac5cba72f26cb1e8aa2060661e792274
Teale Fristoe <fristoe@gmail.com>
2025-01-28 07:56:25 -0800

    Separate MapPanControls.

--- src/components/ui/graph-ui.tsx
+++ src/components/ui/graph-ui.tsx
@@ -1,9 +1,7 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
 import LegendIcon from "../../assets/icons/display-hide-legend-icon.svg";
-import MapControlsIcon from "../../assets/icons/display-hide-map-controls-icon.svg";
 import HomeIcon from "../../assets/icons/home-icon.svg";
-import MapPanArrowIcon from "../../assets/icons/map-pan-arrow-icon.svg";
 import MapResetIcon from "../../assets/icons/map-reset-icon.svg";
 import MapZoomInIcon from "../../assets/icons/map-zoom-in-icon.svg";
 import MapZoomOutIcon from "../../assets/icons/map-zoom-out-icon.svg";
@@ -12,7 +10,7 @@ import PointIcon from "../../assets/icons/point-selection.svg";
 import { dstCamera } from "../../models/camera";
 import { graph } from "../../models/graph";
 import { ui } from "../../models/ui";
-import { ArrowButton } from "./arrow-button";
+import { MapPanControls } from "./map-pan-controls";
 import { NavigationControls } from "./navigation-controls/navigation-controls";
 import { UIButton } from "./ui-button";
 import { UIButtonContainer } from "./ui-button-container";
@@ -47,6 +45,7 @@ export const GraphUI = observer(function GraphUI() {
           testId="button-map-zoom-out"
         />
       </UIButtonContainer>
+      <MapPanControls />
       <UIButtonContainer className="map-reset-container">
         <UIButton
           className="top bottom"
@@ -83,39 +82,6 @@ export const GraphUI = observer(function GraphUI() {
           testId="button-marquee-mode"
         />
       </UIButtonContainer>
-      <div className="map-arrow-container" style={{ rotate: `${-dstCamera.rotation * 180 / Math.PI}deg` }}>
-        <div className="center-map-container">
-          <MapControlsIcon className="center-map" />
-        </div>
-        <ArrowButton
-          className="map-arrow"
-          direction="left"
-          disabled={!graph.canPanLeft}
-          Icon={MapPanArrowIcon}
-          onClick={() => graph.panLeft()}
-        />
-        <ArrowButton
-          className="map-arrow"
-          direction="right"
-          disabled={!graph.canPanRight}
-          Icon={MapPanArrowIcon}
-          onClick={() => graph.panRight()}
-        />
-        <ArrowButton
-          className="map-arrow"
-          direction="up"
-          disabled={!graph.canPanUp}
-          Icon={MapPanArrowIcon}
-          onClick={() => graph.panUp()}
-        />
-        <ArrowButton
-          className="map-arrow"
-          direction="down"
-          disabled={!graph.canPanDown}
-          Icon={MapPanArrowIcon}
-          onClick={() => graph.panDown()}
-        />
-      </div>
     </>
   );
 });
