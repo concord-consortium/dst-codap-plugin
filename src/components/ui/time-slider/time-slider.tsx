552d5cea7817379ee5a03c9e870163c5d6d48d59
Teale Fristoe <fristoe@gmail.com>
2025-01-28 17:54:22 -0800

    Add play button.

--- src/components/ui/time-slider/time-slider.tsx
+++ src/components/ui/time-slider/time-slider.tsx
@@ -1,8 +1,12 @@
 import { observer } from "mobx-react-lite";
 import React, { useEffect, useState } from "react";
 import MapSlider from "../../../assets/icons/map-slider.svg";
+import PauseIcon from "../../../assets/icons/pause-icon.svg";
+import PlayIcon from "../../../assets/icons/play-icon.svg";
 import TimeSliderThumb from "../../../assets/icons/time-slider-thumb.svg";
 import { formatDateString } from "../../../utilities/date-utils";
+import { UIButton } from "../ui-button";
+import { UIButtonContainer } from "../ui-button-container";
 import { DateRangeSliderThumb } from "./date-range-slider-thumb";
 import { SliderThumb } from "./slider-thumb";
 import { TimeLine } from "./time-line";
@@ -12,6 +16,9 @@ import {
 import "./time-slider.scss";
 
 const labelBaseTop = timeLineTop - labelHeight / 2;
+let lastFrameTime = Date.now();
+const animationRate = 0.1;
+let animationTimeout = 0;
 
 interface ITimeSliderProps {
   dateMax: number;
@@ -22,11 +29,39 @@ export const TimeSlider = observer(function TimeSlider({ dateMax, dateMin }: ITi
   const [lowerRangePercent, setLowerRangePercent] = useState(0);
   const [mapPercent, setMapPercent] = useState(0);
   const [timePercent, setTimePercent] = useState(1);
+  const [animating, setAnimating] = useState(false);
 
   useEffect(() => {
     setMapPercent(value => Math.min(upperRangePercent, Math.max(lowerRangePercent, value)));
     setTimePercent(value => Math.min(upperRangePercent, Math.max(lowerRangePercent, value)));
   }, [upperRangePercent, lowerRangePercent]);
+
+  useEffect(() => {
+    if (animating && !animationTimeout) {
+      animationTimeout = setTimeout(() => {
+        animationTimeout = 0;
+        const now = Date.now();
+        const delta = (now - lastFrameTime) / 1000;
+        lastFrameTime = now;
+        const newMapPercent = Math.min(timePercent + animationRate * delta, upperRangePercent);
+        setTimePercent(newMapPercent);
+        if (newMapPercent >= upperRangePercent) {
+          setAnimating(false);
+        }
+      });
+    }
+  }, [animating, timePercent, upperRangePercent]);
+
+  const handlePlayButtonClick = () => {
+    if (animating) {
+      setAnimating(false);
+      clearTimeout(animationTimeout);
+      animationTimeout = 0;
+    } else {
+      setAnimating(true);
+      lastFrameTime = Date.now();
+    }
+  };
 
   const labelFromPercentage = (percentage: number) => {
     const dateValue = dateMin + (dateMax - dateMin) * percentage;
@@ -91,6 +126,16 @@ export const TimeSlider = observer(function TimeSlider({ dateMax, dateMin }: ITi
         topOffset={timeSliderThumbOffset}
         ThumbIcon={TimeSliderThumb}
       />
+      <UIButtonContainer className="play-container">
+        <UIButton
+          className="play-button top bottom"
+          disabled={timePercent >= upperRangePercent}
+          Icon={animating ? PauseIcon : PlayIcon}
+          onClick={handlePlayButtonClick}
+          testId="button-play"
+        />
+      </UIButtonContainer>
+      <div className="play-button-label">{animating ? "Pause" : "Play"}</div>
     </div>
   );
 });
