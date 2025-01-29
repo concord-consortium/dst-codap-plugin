0828e640b0e0e68a3dc932faca3f54ffb118813b
Teale Fristoe <fristoe@gmail.com>
2025-01-29 07:01:41 -0800

    Use d3 range for quick array.

--- src/components/ui/time-slider/time-slider-contsants.ts
+++ src/components/ui/time-slider/time-slider-contsants.ts
@@ -1,9 +1,11 @@
+import { range } from "d3";
+
 export const timeLineTop = 48;
 export const timeLineHeight = 240;
 
 const numTicks = 7;
 const tickSpacing = timeLineHeight / (numTicks + 1);
-export const tickOffsets = Array.from({ length: numTicks }, (_, i) => (i + 1) * tickSpacing);
+export const tickOffsets = range(1, numTicks + 1).map(num => num * tickSpacing);
 export const labelOffsets = [0, ...tickOffsets, timeLineHeight];
 
 export const labelHeight = 18;
