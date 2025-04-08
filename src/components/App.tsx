dede689c2b5a8ba1b29244e5ff565d10aaa4b57d
Teale Fristoe <fristoe@gmail.com>
2025-04-08 10:52:33 -0700

    Dstcube-9 About Tab (#26)
    * Add and style about text.
    
    * Resize plugin on tab change.
    
    * Add tab hover styling.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
 import { ChakraProvider, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import React, { useEffect } from "react";
-import { initializeDST } from "../utilities/codap-utils";
-import { kAboutTabLabel, kGraphTabLabel } from "../utilities/constants";
+import { initializeDST, resizePlugin } from "../utilities/codap-utils";
+import {
+  kAboutTabHeight, kAboutTabLabel, kAboutTabWidth, kGraphTabHeight, kGraphTabLabel, kGraphTabWidth
+} from "../utilities/constants";
 import { AboutTab } from "./about-tab";
 import "./App.css";
 import { GraphTab } from "./graph-tab";
@@ -11,10 +13,16 @@ export const App = () => {
     initializeDST();
   }, []);
 
+  const handleTabChange = (index: number) => {
+    const height = [kGraphTabHeight, kAboutTabHeight][index];
+    const width = [kGraphTabWidth, kAboutTabWidth][index];
+    resizePlugin(height, width);
+  };
+
   return (
     <ChakraProvider>
       <div className="App">
-        <Tabs variant="enclosed">
+        <Tabs variant="enclosed" onChange={handleTabChange}>
           <TabList>
             <Tab>
               {kGraphTabLabel}
